265b4452e4e947287f419b277e440ea68f162c9a
Cheyenne Jones <cj1011012020@gmail.com>
2018-10-02 09:12:48 -0500

    added code

--- Chapter6/src/excercises/TestScoreStatistics.java
+++ Chapter6/src/excercises/TestScoreStatistics.java
@@ -1,16 +1,40 @@
 package excercises;
 
+import java.util.Scanner;
+
 public class TestScoreStatistics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		int userEntry;
-		int count;
-		int total;
-		int avgTotal;
+		Scanner input = new Scanner(System.in);
+		int userChoice;
+		int count = 0;
+		int high = 0;
+		int low = 100;
+		int avg;
+		int total = 0;
+		System.out.print("Enter a number to exit enter 999 >>>");
+		userChoice = input.nextInt();
 		
-		
+		do
+		{
+			if(userChoice > high) 
+			{
+				high = userChoice;
+				count = count + 1;
+			}
+			else if(userChoice < low) 
+			{
+				low = userChoice;
+				count = count + 1;
+			}
+			else if(userChoice > 100 || userChoice < 0) 
+			{
+				System.out.println("Out of range");
+			}
+			total = total + userChoice;
+			avg = total/count;
+		}while(userChoice != 999);
 	}
 
 }
