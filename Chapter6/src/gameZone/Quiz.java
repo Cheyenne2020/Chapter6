f9d35bd9441cece0cf54dca48d099d6b5479c467
Cheyenne Jones <cj1011012020@gmail.com>
2018-10-02 09:49:21 -0500

    added code

--- Chapter6/src/gameZone/Quiz.java
+++ Chapter6/src/gameZone/Quiz.java
@@ -1,10 +1,21 @@
 package gameZone;
 
+import java.util.Scanner;
+
 public class Quiz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		int userChoice;
+		int choiceA;
+		int choiceB;
+		int choiceC;
+		int choiceD;
+		int total;
+		
+		System.out.println("What is the popular song ?  >>>");
+		userChoice = input.nextInt();
 	}
 
 }
